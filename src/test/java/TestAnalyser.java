3abc45cab91c9d57de906eeecc637122bc8d0885
ALLOJU <mounika.alloju@gmail.com>
2022-03-26 16:19:30 +0530

    UC2 with state code data added

--- src/test/java/TestAnalyser.java
+++ src/test/java/TestAnalyser.java
@@ -3,16 +3,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import censusanalyser.CSVStateCode;
 import censusanalyser.InvalidDelimiter;
 import censusanalyser.InvalidFile;
 import censusanalyser.StateCensusAnalyser;
 
 public class TestAnalyser {
 	StateCensusAnalyser analyser;
-
+	CSVStateCode codeAnalyser;
 	@Before
 	public void initialization() {
 		analyser = new StateCensusAnalyser();
+		codeAnalyser = new CSVStateCode();
 	}
 
 	@Test
@@ -24,17 +26,26 @@ public class TestAnalyser {
 	public void testFileCheckSad() throws InvalidFile, InvalidDelimiter {
 		analyser.loadData("src/test/resources/IndiaStateCensus.csv");
 	}
+	
 	@Test
 	public void testTypeCheckSad() throws InvalidFile, InvalidDelimiter {
 		analyser.loadData("src/test/resources/IndiaStateCensusDataWrong.csv");
 	}
+	
 	@Test
 	public void testDelimiterCheckSad() throws InvalidFile, InvalidDelimiter  {
 		analyser.loadData("src/test/resources/IndiaStateCensusDataWrongDeliminator.csv");
 	}
+	
 	@Test
 	public void testHeaderCheckSad() throws InvalidFile, InvalidDelimiter {
 		analyser.loadData("src/test/resources/IndiaStateCensusData.csv");
+	}
+	
+	@Test
+	public void testCodeRecordMatch() throws Exception {
+		codeAnalyser.loadData("src/test/resources/IndiaStateCode.csv");
+		assertEquals(true, codeAnalyser.checkData(37));
 	}
 	
 }
